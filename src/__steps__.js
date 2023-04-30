93b83c38d3df732129c01b691ffe6cf34b7fc6c8
evo264714 <mh264714@gmail.com>
2023-04-30 19:32:58 +0600

    third

--- src/__steps__.js
+++ src/__steps__.js
@@ -5,7 +5,14 @@
 * 4. save firebase config and export default app
 * 5. Build > Authentication > get started > enable sign in method
 * 6. create sign up and log in route 
-* 
+*/
+
+/*
+* 1. Create a context provider
+* 2. create a context and set provider
+* 3. set the children props
+* 4. set context value
+* 5. set a provider
 * 
 * 
 * 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
+import { useContext } from 'react';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset();
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handleLogin}>
+                <div className='form-control'>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required placeholder='Your Email' />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" required placeholder='Your Password' />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p><small>New to Ema John?<Link to='/signup'> Create a new account</Link> </small></p>
         </div>
     );
 };
